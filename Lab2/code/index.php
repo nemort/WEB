e179deb28d7a8c3293bd3c453c55c326ac49922d
nemort <n3m0rt@gmail.com>
2024-03-10 01:04:59 +0200

    [lab 2][task 3] Showed the operation of integer and floating point variables, as well as subtraction.

--- Lab2/code/index.php
+++ Lab2/code/index.php
@@ -4,8 +4,27 @@ $very_bad_unclear_name = "15 chicken wings";
 
 
 // Write your code here:
+//1
 $order = &$very_bad_unclear_name;
 $order .= ", coke and fries";
+
+
+//2
+$count = 3;
+echo $count;
+echo "\n";
+
+$cost = 10.9;
+echo "$cost";
+echo "\n";
+
+echo 1+1+1+1+1+1+1+1+1+1+1+1;
+echo "\n";
+
+$lastMonth = 1187.23;
+$thisMonth = 1089.98;
+echo "Разница между расходами в этом месяце и прошлом ", $lastMonth - $thisMonth, "\n";
+
 
 // Don't change the line below
 echo "\nYour order is: $very_bad_unclear_name.";
