889e8647f5ed5c01c7758afda2aa73f0a22aad01
nemort <n3m0rt@gmail.com>
2024-03-10 01:13:23 +0200

    [lab 2][task 4] Calculated how many days Meg took to learn each language on average.

--- Lab2/code/index.php
+++ Lab2/code/index.php
@@ -23,7 +23,16 @@ echo "\n";
 
 $lastMonth = 1187.23;
 $thisMonth = 1089.98;
-echo "Разница между расходами в этом месяце и прошлом ", $lastMonth - $thisMonth, "\n";
+echo "Разница между расходами в этом месяце и прошлом ", $lastMonth - $thisMonth, "\n\n";
+
+
+//11
+$num_language = 4;
+$months = 11;
+$days = $months * 16;
+$days_per_language = $days / $num_language;
+echo $days_per_language;
+echo "\n";
 
 
 // Don't change the line below
