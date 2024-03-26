4019a7ca1ddf363b567398da7afca73ce2753a22
nemort <n3m0rt@gmail.com>
2024-03-26 23:25:13 +0200

    [lab 3][task 1][b] Converted the string using a regular expression.

--- Lab3/task 1/index.php
+++ Lab3/task 1/index.php
@@ -6,3 +6,9 @@ $regexp = '/a.{2}b/i';
 $matches = [];
 $count = preg_match_all($regexp, $str, $matches);
 echo $count, "\n";
+
+//b.
+$str = 'a1b2c3';
+$regexp = '/[0-9]+/';
+$result = preg_replace_callback($regexp, function($match) {return $match[0]**3;}, $str);
+echo $result;
